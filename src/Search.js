aeb855f935adcfbc81d5bfd7b58d68b40324df57
Ahmed Fekry <ahmedfikry78@gmail.com>
2022-04-19 01:36:49 +0200

    added search component and listbooks component and added routes dom

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export default class Search extends Component {
   render() {
@@ -6,7 +7,7 @@ export default class Search extends Component {
       <div>
           <div className="search-books">
               <div className="search-books-bar">
-                <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+                <Link className="close-search" to="/">Close</Link>
                 <div className="search-books-input-wrapper">
                   {/*
                     NOTES: The search from BooksAPI is limited to a particular set of search terms.
--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
 
-ReactDOM.render( <BrowserRouter><App /></BrowserRouter> , document.getElementById('root'))
+import Search from './Search'
+import ListBooks from './ListBooks'
+
+ReactDOM.render( <BrowserRouter>
+                    <Routes>
+                        <Route path='/' element={<App />} >
+                        </Route>
+                        <Route path='/search' element={<Search />}></Route>
+                    </Routes>
+                 </BrowserRouter> 
+                 , document.getElementById('root'))
